d8684eefc1d0941563e7d1939e60efbe9f69fd7e
leite <xxleite@gmail.com>
2013-07-14 19:12:07 -0300

    adjusting to support oauth

--- everyauth.lua
+++ everyauth.lua
@@ -5,16 +5,68 @@
 -- this stuff is worth it, you can buy me a beer in return
 -- ----------------------------------------------------------------------------
 
-local string    = require 'string'
-local json      = require 'json'
-local coroutine = require 'coroutine'
-local http      = require 'musasaua'
+-- i had lots of inspiration from: 
+-- lua-oauth (https://github.com/DracoBlue/lua-oauth)
+-- everyauth (https://github.com/bnoguchi/everyauth)
 
-local sub, gsub, format, find, byte, match, upper, len, parse, create, resume, yield 
-    = string.sub, string.gsub, string.format, string.find, string.byte, string.match, string.upper,
-      string.len, json.parse, coroutine.create, coroutine.resume, coroutine.yield
+local s, t, js, c, http, m, o, hx, b64 =
+      require('string'), require('table'), require('json'), require('coroutine'),
+      require('musasaua'), require('math'), require('os'), require('_crypto').hmac, require('luvit-base64')
 
-string, json, coroutine = nil, nil, nil
+local sub, gsub, format, find, byte, match, upper, len, char, parse, create, resume, yield, digest, random, seed, time, insert, sort, enc64, dec64 = 
+      s.sub, s.gsub, s.format, s.find, s.byte, s.match, s.upper, s.len, s.char, js.parse, c.create, c.resume, c.yield, hx.digest, m.random,
+      m.randomseed, o.time, t.insert, t.sort, b64.encode, b64.decode
+
+s, m, js, c, m, o, hx, b64 = nil, nil, nil, nil, nil, nil, nil, nil
+
+local oauth_params = {
+    oauth_consumer_key     = true,
+    oauth_nonce            = true,
+    oauth_signature_method = true,
+    oauth_token            = true,
+    oauth_verifier         = true,
+    oauth_timestamp        = true,
+    oauth_callback         = true,
+    oauth_version          = true,
+    scope                  = true
+  }
+
+-- basic functions
+local function encode_hexa(c)
+  return format('%%%02X', byte(c))
+end
+
+local function decode_hexa(c)
+  return char(tonumber(c, 16))
+end
+
+local function sort_it(a, b)
+  return a.k==b.k and (a.v<b.v) or (a.k<b.k)
+end
+
+local function sha1(...)
+  local args = ...
+  return digest('sha1', args[1], args[2], args[3])
+end
+
+local function nonce()
+  seed(time())
+  return sha1(tostring(random()) .. 'random' .. tostring(time()), 'keyyyy')
+end
+
+local function encode_param(str)
+  if not str then
+    return ''
+  end
+  return gsub(gsub(gsub(str, '\n', '\r\n'), '([^%w])', encode_hexa), ' ', '+') --([^-%._~%w])
+end
+
+local function decode_param(str)
+  if not str then
+    return ''
+  end
+  return gsub(gsub(gsub(str, '+', ' '), '%%(%x%x)', decode_hexa), "\r\n", "\n")
+end
 
 local everyauth, debug = {}, false
 everyauth.__index = everyauth
@@ -25,9 +77,12 @@ local function debug(...)
   end
 end
 
-everyauth.auth_path                = '/oauth/authorize'
-everyauth.access_token_path        = '/oauth/access_token'
-everyauth.access_token_http_method = 'POST'
+everyauth.uses                     = 'oauth2'               -- default oauth2
+everyauth.auth_path                = '/oauth/authorize'     -- oauth2
+everyauth.access_token_path        = '/oauth/access_token'  -- oauth/oauth2
+everyauth.access_token_http_method = 'POST'                 
+everyauth.authorize_path           = '/oauth/authenticate'  -- oauth
+everyauth.request_token_path       = '/oauth/request_token' -- oauth 
 everyauth.oauth_coroutine          = nil
 
 -- custom parser, luvit parse is not good
@@ -46,21 +101,47 @@ function everyauth:get_query(url)
   return results
 end
 
--- 
-function everyauth:stringify_query(query_array)
-  local function urlencode(str)
-    if not str then
-      return ''
-    end
-    return gsub(gsub(gsub(str, '\n', '\r\n'), '([^%w])', function(c) return format('%%%02X', byte(c)) end), ' ', '+')
+-- oauth way self:stringify_query({k,v}) >>> returns query string
+-- oauth2 way self:stringify_query({k,v}, true) >>> returns query headers, query string and all params
+-- stringfy a hash o query values (query_hash, [sort and sort_out])
+function everyauth:stringify_query(...)
+  local query, query_array, sort_sort_out = '', ...
+  if not query_array then
+    return query
   end
-  local query = ''
-  if query_array then
+    --
+  if sort_sort_out then
+    local _query_table, query_params, all_params = {}, '', ''
+    --
     for key, value in pairs(query_array) do
-      query = query .. urlencode(key) ..'='.. urlencode(value) ..'&'
+      insert(
+        _query_table, 
+        {
+          k = encode_param(key),
+          v = encode_param(value)
+        })
     end
+    --
+    sort(_query_table, sort_it)
+    -- 
+    for i=1, #_query_table do
+
+      if oauth_params[_query_table[i].k] then
+        query = format('%s%s="%s", ', query, _query_table[i].k, _query_table[i].v)
+      else
+        query_params = format('%s%s=%s&', query_params, _query_table[i].k, _query_table[i].v)
+      end
+      all_params = format('%s%s=%s&', all_params, _query_table[i].k, _query_table[i].v)
+    end
+    return sub(query, 1, -3), 
+           sub(query_params, 1, -2), 
+           sub(all_params, 1, -2)
   end
-  return query
+  --
+  for key, value in pairs(query_array) do
+    query = format('%s%s=%s&', query, encode_param(key), encode_param(value))
+  end
+  return sub(query, 1, -2)
 end
 
 -- request oauth endpoint (url, callback, [params], [method], [headers], [timeout])
@@ -73,23 +154,28 @@ function everyauth:request(url, callback, ...)
     (args[3] and (type(args[3])=='table'  and args[3] or {}   ) or {})
 
   local function parse_data(data)
+    p('returning ...')
+    --p(data)
     if not data then
       callback(true, nil)
       return
     end
 
     if sub(data.headers['content-type'], 1, 16)=='application/json' then
-      callback(false, parse(data.body))
+      callback(false, parse(data.body or data.data))
     elseif sub(data.headers['content-type'], 1, 10)=='text/plain' or sub(data.headers['content-type'], 1, 33)=='application/x-www-form-urlencoded' then
-      callback(false, self:get_query(data.body))
+      callback(false, self:get_query(data.body or data.data))
     else
-      callback(pcall(parse, data.body))  -- try to parse
+      callback(pcall(parse, data.body or data.data))  -- try to parse
     end
   end
 
-  http.connection_timeout = timeout
-  http.read_timeout       = timeout
-  http:request({url = url, method = method, params = params, headers = headers}, parse_data)
+  p('request ...')
+
+  local xhttp = http:new()
+  xhttp.connection_timeout = timeout
+  xhttp.read_timeout       = timeout
+  xhttp:request({url = url, method = method, params = params, headers = headers}, parse_data)
 end
 
 -- 
@@ -110,15 +196,10 @@ end
 
 -- 
 function everyauth:get_code(params)
-  if self:auth_callback_did_err(params) then
+  if not params.code or self:auth_callback_did_err(params) then
     debug('get code :: fail')
     return self:handle_auth_callback_error(params)
   end
-
-  if not params.code then
-    error("Missing code in querystring. The url looks like " + req.url)
-  end
-  
   return params.code
 end
 
@@ -150,8 +231,53 @@ function everyauth:get_access_token(code, callback)
   
 end
 
--- server side oauth authentication sequence
+function everyauth:get_oauth_sign(params)
+  --
+  local token_secret, method, headers, url, auth_query, query_params, all_query_params, hmac_b64 =
+        (params.oauth_token_secret or ''), (params.method or 'GET'), (params.headers or {}), params.url, '', '', '', ''
+  
+  --
+  params.oauth_signature_method = params.oauth_signature_method or 'HMAC-SHA1'
+  params.url, params.method, params.oauth_token_secret, params.headers = nil, nil, nil, nil
+
+  -- returns query headers, query string and all params
+  auth_query, query_params, all_query_params = self:stringify_query(params, true)
+
+  -- 
+  hmac_b64 = enc64(
+              sha1(
+                format(
+                  "%s&%s&%s", 
+                  method, 
+                  encode_param(url), 
+                  all_query_params
+                ),
+                format(
+                  "%s&%s", 
+                  encode_param(self.app_secret), 
+                  encode_param(token_secret)
+                ),
+                true
+              ))
+  
+  --
+  insert(
+    headers, 
+    format(
+      'Authorization: OAuth "%s", oauth_signature="%s"',
+      auth_query,
+      encode_parameter(hmac_b64)
+    ))
+
+  return (method=='GET' and url ..'?' or '') .. (len(query_params)>3 and query_params or ''), headers
+end
+
 function everyauth:oauth_transaction(params)
+
+end
+
+-- server side oauth2 authentication sequence
+function everyauth:oauth2_transaction(params)
   local code, status, data, user_params, err_callback 
     = nil, false, {}, {}, 
     function(data)
@@ -165,8 +291,11 @@ function everyauth:oauth_transaction(params)
       return true
     end
 
+    p('get code')
   -- get authentication code
-  code = self:get_code(params)          
+  code = self:get_code(params) 
+
+  p('code fetched', code)         
   -- get access token
   self:get_access_token(code, function(err, returned_data)
     debug('access token', err, returned_data)
@@ -217,14 +346,20 @@ function everyauth:middleware(options)
     local params   = req.uri.search and self:get_query(req.uri.search) or {}
     self.req, self.res, self.nxt, self.host_name = req, res, nxt, "http://" .. req.headers.host
 
-    p(self.entry_path, self.callback_path, req.uri.pathname, match(req.uri.pathname, self.entry_path), match(req.uri.pathname, self.callback_path))
+    --p(req.uri)
+    --p(self.entry_path, self.callback_path, req.uri.pathname, match(req.uri.pathname, self.entry_path), match(req.uri.pathname, self.callback_path))
 
-    if match(req.uri.pathname, self.entry_path) then   
+    if match(req.uri.pathname, self.entry_path..'/?$') then   
       -- request authentication
+      p(' entry path match ... '.. self.entry_path)
       res:send(302, nil, {['Location'] = self:get_auth_uri()}, true)
-    elseif match(req.uri.pathname, self.callback_path) then  
+    elseif match(req.uri.pathname, self.callback_path..'/?$') then  
       -- authentication callback
-      self.oauth_coroutine = create(function() self:oauth_transaction(params) end)
+      p(' callback path match ... '.. self.callback_path)
+      
+      self.oauth_coroutine = self.uses=='oauth' and
+                             create(function() self:oauth_transaction(params) end) or
+                             create(function() self:oauth2_transaction(params) end)
       resume(self.oauth_coroutine)
     else
       nxt()
--- twitter.lua
+++ twitter.lua
@@ -7,12 +7,23 @@ local conf = {
   app_id        = "",
   app_secret    = "",
   mobile        = false,
-
+  uses          = 'oauth'
   api_host      = "https://api.twitter.com",
   oauth_host    = "https://api.twitter.com",
-  auth_path     = "/oauth2/token",
   
-  access_token_path     = '/oauth2/token',
+
+  --consumer_key = "AAAAAAAAAAAAAAAA", -- take that from your twitter app page
+  --consumer_secret = "BBBBBBBBBBBBBBBB", -- take that from your twitter app page
+  
+  --      request_token_url = 'https://api.twitter.com/oauth/request_token',
+  --      authorize_url= 'https://api.twitter.com/oauth/authorize',
+  --      access_token_url= 'https://api.twitter.com/oauth/access_token',
+  --      token_ready_url='http://example.org/oauth/token_ready'
+
+  authorize_path     = '/oauth/authenticate'
+  request_token_path = '/oauth/request_token'
+  access_token_path  = '/oauth/access_token'
+
   auth_callback_did_err = function(self, params)
     return params and params.error
   end
@@ -31,10 +42,11 @@ conf.access_token_params = function(self)
     return {grant_type = 'authorization_code'}
   end
 
-conf.user_params = function(self, access_token)
+conf.user_params = function(self, ...)
+    local access_token, access_token_secret = {...}
     return {
         url    = conf.api_host .. '/1.1/users/show.json',
-        params = {access_token = access_token}
+        params = {access_token = access_token, access_token_secret = access_token_secret}
       }
   end
 
